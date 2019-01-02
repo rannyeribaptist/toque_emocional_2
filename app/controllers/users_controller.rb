65cad892d9ac5734b840141b68a35f7db1e29e76
RannyeriBatista <rannyeri@protonmail.com>
2019-01-02 07:57:32 -0300

    users views and polished columns scheme

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -72,6 +72,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:school_id, :role)
+      params.require(:user).permit(:school_id, :role, :email, :password, :password_confirmation)
     end
 end
