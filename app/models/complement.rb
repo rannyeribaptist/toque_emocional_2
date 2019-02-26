b2bd5dae033b117414d02f2ebc2108976ad56d65
RannyeriBatista <rannyeri@protonmail.com>
2019-02-26 11:13:33 -0300

    changed uploader for complements

--- app/models/complement.rb
+++ app/models/complement.rb
@@ -2,5 +2,5 @@ class Complement < ApplicationRecord
   belongs_to :book
 
   validates_presence_of :name, :file
-  mount_uploader :file, ArchiveUploader
+  mount_uploader :file, ComplementUploader
 end
